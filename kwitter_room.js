076e34afabde91c3a5de43a7fadc3f86fc44d5ac
LisaDash10 <81564491+LisaDash10@users.noreply.github.com>
2021-06-03 12:46:03 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA3O6xfGzvJPnxqvVMkB7oeiK3rlhVqYBs",
+    authDomain: "lets-chat-project-e10ab.firebaseapp.com",
+    databaseURL: "https://lets-chat-project-e10ab-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-project-e10ab",
+    storageBucket: "lets-chat-project-e10ab.appspot.com",
+    messagingSenderId: "680274294402",
+    appId: "1:680274294402:web:2e46c7efaf9e19725f269b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
